cfa23bbd2fff236c1589c4df3ad7a8f8a5588df2
Nikita <dennikita03@gmail.com>
2019-07-19 21:10:39 +0500

    added testing and other changes

---
+++ test_maxminindex.py
@@ -0,0 +1,36 @@
+import pytest
+import pandas as pd
+
+
+def max_min_index(name_of_index, name):
+    """Returns the maximum and minimum value of a column from df."""
+    max_value = df[name_of_index].max()
+    max_country = list(df[name == max_value]['country'])
+    min_value = df[name_of_index].min()
+    min_country = list(df[name == min_value]['country'])
+    return (max_value, max_country), (min_value, min_country)
+
+
+df = pd.read_csv("city/output/list_of_countries.csv")
+
+
+@pytest.mark.parametrize("name_index, name", [
+    ('safety_index', df.safety_index),
+    ('cost_of_living_index', df.cost_of_living_index),
+    ('property_price_to_income_ratio', df.property_price_to_income_ratio),
+    ('health_care_index', df.health_care_index),
+    ('purchasing_power_index', df.purchasing_power_index),
+    ('traffic_commute_time_index', df.traffic_commute_time_index),
+    ('pollution_index', df.pollution_index),
+])
+def test_max_min_index(name_index, name):
+    l = []
+    for i in df[name_index]:
+        if i != 'nan':
+            l.append(i)
+    max_value = max(l)
+    min_value = min(l)
+    max_country = list(df[name == max_value]['country'])
+    min_country = list(df[name == min_value]['country'])
+    assert max_min_index(name_index, name) == ((max_value, max_country),
+                                               (min_value, min_country))
--- where.py
+++ where.py
@@ -4,21 +4,47 @@ import pandas as pd
 df = pd.read_csv("city/output/list_of_countries.csv")
 
 
-def max_min_index(name_of_index):
+def max_min_index(name_of_index, name):
     """Returns the maximum and minimum value of a column from df."""
     max_value = df[name_of_index].max()
+    max_country = list(df[name == max_value]['country'])
     min_value = df[name_of_index].min()
-    return max_value, min_value
+    min_country = list(df[name == min_value]['country'])
+    return (max_value, max_country), (min_value, min_country)
 
 
-max_min_purchasing = max_min_index('purchasing_power_index')
-max_min_safety = max_min_index('safety_index')
-max_min_health = max_min_index('health_care_index')
-max_min_cost = max_min_index('cost_of_living_index')
-max_min_property = max_min_index('property_price_to_income_ratio')
-max_min_traffic = max_min_index('traffic_commute_time_index')
-max_min_pollution = max_min_index('pollution_index')
-max_min_climate = max_min_index('climate_index')
+def print_question(name_index, max_min_value, mood):
+    """Returns a question that asks the user about the status of a factor
+    in his country,also displays the maximum and minimum value of this factor
+    in the world."""
+    if mood == 'higher is better':
+        first_value = max_min_value[0]
+        second_value = max_min_value[1]
+    elif mood == 'lower is better':
+        first_value = max_min_value[1]
+        second_value = max_min_value[0]
+    message = (
+        f"What is your {name_index} ({mood})? "
+        f"The best score in the world is "
+        f"{first_value[0]} "
+        f"({first_value[1][0]}), "
+        f"the worst is {second_value[0]} "
+        f"({second_value[1][0]}) "
+    )
+    return message
+
+
+max_min_purchasing = max_min_index('purchasing_power_index',
+                                   df.purchasing_power_index)
+max_min_safety = max_min_index('safety_index', df.safety_index)
+max_min_health = max_min_index('health_care_index', df.health_care_index)
+max_min_cost = max_min_index('cost_of_living_index', df.cost_of_living_index)
+max_min_property = max_min_index('property_price_to_income_ratio',
+                                 df.property_price_to_income_ratio)
+max_min_traffic = max_min_index('traffic_commute_time_index',
+                                df.traffic_commute_time_index)
+max_min_pollution = max_min_index('pollution_index', df.pollution_index)
+max_min_climate = max_min_index('climate_index', df.climate_index)
 
 your_country = input("What is your country? ")
 your_country = your_country.lower()
@@ -27,51 +53,47 @@ your_country = your_country.capitalize()
 your_purchasing_power_index = float(
     df[df.country == your_country]["purchasing_power_index"]
 )
-print(f"In your country purchasing power index is {your_purchasing_power_index}")
-
+print(
+    f"In your country purchasing power index is {your_purchasing_power_index}"
+    )
 your_purchasing_power_index = (
-    float(input(f"What is your purchasing power index (higher is better)? "
-                f"The best score in the world is "
-                f"{max_min_purchasing[0]} "
-                f"({list(df[df.purchasing_power_index == max_min_purchasing[0]]['country'])[0]}), "
-                f"the worst is {max_min_purchasing[1]} "
-                f"({list(df[df.purchasing_power_index == max_min_purchasing[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('purchasing power index', max_min_purchasing,
+                       'higher is better')
+    )) or
     your_purchasing_power_index
     )
 
-your_safety_index = float(df[df.country == your_country]["safety_index"])
+your_safety_index = float(
+    df[df.country == your_country]["safety_index"]
+)
 print(f"In your country safety index is {your_safety_index}")
 your_safety_index = (
-    float(input(f"What is your safety index (higher is better)? "
-                f"The best score in the world is "
-                f"{max_min_purchasing[0]} "
-                f"({list(df[df.safety_index == max_min_safety[0]]['country'])[0]}), "
-                f"the worst is {max_min_purchasing[1]} "
-                f"({list(df[df.safety_index == max_min_safety[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('safety index', max_min_safety, 'higher is better')
+    )) or
     your_safety_index
 )
 
-your_health_care_index = float(df[df.country == your_country]["health_care_index"])
+your_health_care_index = float(
+    df[df.country == your_country]["health_care_index"]
+)
 print(f"In your country health care index is {your_health_care_index}")
 your_health_care_index = (
-    float(input("What is your health care index (higher is better)? "
-                f"The best score in the world is "
-                f"{max_min_health[0]} "
-                f"({list(df[df.health_care_index == max_min_health[0]]['country'])[0]}), "
-                f"the worst is {max_min_health[1]} "
-                f"({list(df[df.health_care_index == max_min_health[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('health care index', max_min_health, 'higher is better')
+    )) or
     your_health_care_index
 )
 
-your_climate_index = float(df[df.country == your_country]["climate_index"])
+your_climate_index = float(
+    df[df.country == your_country]["climate_index"]
+)
 print(f"In your country climate index is {your_climate_index}")
 your_climate_index = (
-    float(input("What is your climate index (higher is better)? "
-                f"The best score in the world is "
-                f"{max_min_climate[0]} "
-                f"({list(df[df.climate_index == max_min_climate[0]]['country'])[0]}), "
-                f"the worst is {max_min_climate[1]} "
-                f"({list(df[df.climate_index == max_min_climate[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('climate index', max_min_climate, 'higher is better')
+    )) or
     your_climate_index
 )
 
@@ -80,12 +102,9 @@ your_cost_of_living_index = float(
 )
 print(f"In your country cost of living index is {your_cost_of_living_index}")
 your_cost_of_living_index = (
-    float(input("What is your cost of living index (lower is better)? "
-                f"The best score in the world is "
-                f"{max_min_cost[0]} "
-                f"({list(df[df.cost_of_living_index == max_min_cost[0]]['country'])[0]}), "
-                f"the worst is {max_min_cost[1]} "
-                f"({list(df[df.cost_of_living_index == max_min_cost[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('cost of living index', max_min_cost, 'lower is better')
+    )) or
     your_cost_of_living_index
 )
 
@@ -97,12 +116,10 @@ print(
     f"price to income ratio index is {your_property_price_to_income_ratio}"
 )
 your_property_price_to_income_ratio = (
-    float(input("What is your house price to income ratio (lower is better)? "
-                f"The best score in the world is "
-                f"{max_min_property[0]} "
-                f"({list(df[df.property_price_to_income_ratio == max_min_property[0]]['country'])[0]}), "
-                f"the worst is {max_min_property[1]} "
-                f"({list(df[df.property_price_to_income_ratio == max_min_property[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('property price to income ratio', max_min_property,
+                       'lower is better')
+    )) or
     your_property_price_to_income_ratio
 )
 
@@ -110,28 +127,23 @@ your_traffic_commute_time_index = float(
     df[df.country == your_country]["traffic_commute_time_index"]
 )
 print(
-    f"In your country traffic commute time index is {your_traffic_commute_time_index}"
+    f"In your country traffic commute time index is "
+    f"{your_traffic_commute_time_index}"
 )
-
 your_traffic_commute_time_index = (
-    float(input("What is your traffic commute time index (lower is better)? "
-                f"The best score in the world is "
-                f"{max_min_traffic[0]} "
-                f"({list(df[df.traffic_commute_time_index == max_min_traffic[0]]['country'])[0]}), "
-                f"the worst is {max_min_traffic[1]} "
-                f"({list(df[df.traffic_commute_time_index == max_min_traffic[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('traffic commute time index', max_min_traffic,
+                       'lower is better')
+    )) or
     your_traffic_commute_time_index
 )
 
 your_pollution_index = float(df[df.country == your_country]["pollution_index"])
 print(f"In your country pollution index is {your_pollution_index}")
 your_pollution_index = (
-    float(input("What is your pollution index (lower is better)? "
-                f"The best score in the world is "
-                f"{max_min_pollution[0]} "
-                f"({list(df[df.pollution_index == max_min_pollution[0]]['country'])[0]}), "
-                f"the worst is {max_min_pollution[1]} "
-                f"({list(df[df.pollution_index == max_min_pollution[1]]['country'])[0]}) ")) or
+    float(input(
+        print_question('pollution index', max_min_pollution, 'lower is better')
+    )) or
     your_pollution_index
 )
 
@@ -151,7 +163,9 @@ your_purchasing_power_index = float(your_purchasing_power_index)
 your_safety_index = float(your_safety_index)
 your_health_care_index = float(your_health_care_index)
 your_cost_of_living_index = float(your_cost_of_living_index)
-your_property_price_to_income_ratio = float(your_property_price_to_income_ratio)
+your_property_price_to_income_ratio = float(
+    your_property_price_to_income_ratio
+    )
 your_traffic_commute_time_index = float(your_traffic_commute_time_index)
 your_pollution_index = float(your_pollution_index)
 your_climate_index = float(your_climate_index)
@@ -169,10 +183,12 @@ out_df = df[df.purchasing_power_index > your_purchasing_power_index][
 ][
     df.climate_index > your_climate_index
 ]
-print_out_df = out_df[["country", "freedomhouse_score", "quality_of_life_index"]]
+print_out_df = out_df[["country", "freedomhouse_score",
+                       "quality_of_life_index"]]
 
 if print_out_df.empty:
     print(f"Don't exist country better any {your_country}.")
 else:
-    with pd.option_context("display.max_rows", None, "display.max_columns", None):
+    with pd.option_context("display.max_rows", None, "display.max_columns",
+                           None):
         print(print_out_df)
